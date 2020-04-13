d847fbb1ec2e327475b4784f328c2d97924ac2c4
Pedro Portella <pportella23@gmail.com>
2020-04-13 15:21:11 -0300

    react chart

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Chart from './components/Chart'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {  //Problema no construtor, componentWillMount e getChart functions pq é o novo react com hooks e functions (stateless)
+  constructor(){
+    super();
+    this.state = {
+      chartData:{}
+    }
+  }
+
+  componentWillMount(){
+    this.getChartData();
+  }
+
+  getChartData(){
+    // Ajax calls go here! (Aqui que vão as requisições pra API que vão buscar os dados do COVID)
+    this.setState({
+      chartData:{
+        labels: ['Porto Alegre', 'Alegrete', 'Canoas','Novo Hamburgo'],
+        datasets:[{
+            label:'População',
+            data:[
+                1409000,
+                78984,
+                323827,
+                246452
+            ],
+            backgroundColor:[
+                'rgba(255, 99, 132, 0.6)',
+                'rgba(54, 162, 235, 0.6)',
+                'rgba(255, 206, 86, 0.6)',
+                'rgba(75, 192, 192, 0.6)'
+            ]
+        }]
+    }
+    });
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Chart chartData={this.state.chartData} legendPosition="top" />
+      </div>
+    );
+  }
 }
 
 export default App;
