530e0f01ca49fcad7e79ef6bc7e68cb546f5d5e8
rizzotto <guilherme.rizzotto@acad.pucrs.br>
2020-04-13 15:51:30 -0300

    passing to functional components

--- src/App.js
+++ src/App.js
@@ -1,50 +1,43 @@
-import React, {Component} from 'react';
-import './App.css';
-import Chart from './components/Chart'
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Chart from "./components/Chart";
 
-class App extends Component {  //Problema no construtor, componentWillMount e getChart functions pq é o novo react com hooks e functions (stateless)
-  constructor(){
-    super();
-    this.state = {
-      chartData:{}
-    }
-  }
+export default function App() {
+  const [chartData, setCharData] = useState({});
+  const [isBusy, setBusy] = useState(true);
 
-  componentWillMount(){
-    this.getChartData();
-  }
+  useEffect(() => {
+    getChartData();
+  }, []);
 
-  getChartData(){
+  function getChartData() {
     // Ajax calls go here! (Aqui que vão as requisições pra API que vão buscar os dados do COVID)
-    this.setState({
-      chartData:{
-        labels: ['Porto Alegre', 'Alegrete', 'Canoas','Novo Hamburgo'],
-        datasets:[{
-            label:'População',
-            data:[
-                1409000,
-                78984,
-                323827,
-                246452
-            ],
-            backgroundColor:[
-                'rgba(255, 99, 132, 0.6)',
-                'rgba(54, 162, 235, 0.6)',
-                'rgba(255, 206, 86, 0.6)',
-                'rgba(75, 192, 192, 0.6)'
-            ]
-        }]
-    }
+    setCharData({
+      labels: ["Porto Alegre", "Alegrete", "Canoas", "Novo Hamburgo"],
+      datasets: [
+        {
+          label: "População",
+          data: [1409000, 78984, 323827, 246452],
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.6)",
+            "rgba(54, 162, 235, 0.6)",
+            "rgba(255, 206, 86, 0.6)",
+            "rgba(75, 192, 192, 0.6)",
+          ],
+        },
+      ],
     });
+
+    setBusy(false);
   }
 
-  render(){
-    return (
-      <div className="App">
-        <Chart chartData={this.state.chartData} legendPosition="top" />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      {isBusy ? (
+        <p>Loading</p>
+      ) : (
+        <Chart chartData={chartData} legendPosition="top" />
+      )}
+    </div>
+  );
 }
-
-export default App;
--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,39 +1,29 @@
-import React, {Component} from 'react';
-import { Bar, Line, Pie, Doughnut } from 'react-chartjs-2';
+import React, { useState, useEffect } from "react";
+import { Bar, Line, Pie, Doughnut } from "react-chartjs-2";
 
-class Chart extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            chartData:props.chartData
-        }
-    }
+export default function Chart({ chartData, legendPosition }) {
+  const [data, setData] = useState(chartData);
+  const [lPosition, setLPosition] = useState(legendPosition);
 
-    static defaultProps = {
-        displayTitle: true,
-        displayLegend: true,
-        legendPosition: 'right'
-    }
+  const displayTitle = true;
+  const displayLegend = true;
 
-    render(){
-        return (
-            <div className="chart">
-                <Bar
-                    data={this.state.chartData}
-                    options={{
-                        title:{
-                            display:this.props.displayTitle,
-                            text:'População das cidades do RS',
-                            fontSize:25
-                        },
-                        legend:{
-                            display:this.props.displayLegend,
-                            position:this.props.legendPosition
-                        }
-                    }}
-                />
-            </div>
-        )
-    }
+  return (
+    <div className="chart">
+      <Bar
+        data={data}
+        options={{
+          title: {
+            display: displayTitle,
+            text: "População das cidades do RS",
+            fontSize: 25,
+          },
+          legend: {
+            display: displayLegend,
+            position: lPosition,
+          },
+        }}
+      />
+    </div>
+  );
 }
-export default Chart;
